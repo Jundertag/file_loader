ce13c9665174fd90642376e192d751d8c468bd43
Jundertag <jaydenmacarthur3@gmail.com>
2026-05-31 10:24:38 -0300

    directory chosen by user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io;
+use std::io::ErrorKind;
+
 fn main() {
-    println!("Hello, world!");
+    let mut buffer = String::with_capacity(4096);
+    println!("string buffer allocated");
+
+    println!("please type the directory to the file to load into heap memory (e.g., \"C:\\Users\\<user>\\Documents\\file.txt\")");
+
+    match io::stdin().read_line(&mut buffer) {
+        Ok(bytes_read) => {
+            println!("read {bytes_read} bytes")
+        }
+        Err(error) => {
+            println!("failed to read line from console");
+            match error.kind() {
+                ErrorKind::InvalidData => {
+                    
+                }
+
+                _ => {
+
+                }
+            }
+        }
+    }
 }
