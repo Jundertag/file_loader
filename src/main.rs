be701fc9b3e61ef651426889861e6b2771cdaa86
Jundertag <jaydenmacarthur3@gmail.com>
2026-05-31 11:49:35 -0300

    add extra buffer for file contents

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use std::io;
+use std::fs::File;
+use std::{fs, io};
+use std::io::{BufReader, Read};
 use std::io::ErrorKind;
+use std::path::Path;
 
 fn main() {
     let mut buffer = String::with_capacity(4096);
@@ -18,7 +21,7 @@ fn main() {
                 }
 
                 _ => {
-
+                    println!("Unknown error: {:?}", error);
                 }
             }
         }
@@ -26,5 +29,42 @@ fn main() {
 
     let chosen_directory = buffer.trim();
 
+    let file = match File::open(Path::new(chosen_directory)) {
+        Ok(file) => {
+            file
+        }
+        Err(error) => {
+            match error.kind() {
+                ErrorKind::NotFound => {
+                    eprintln!("Path does not exist.");
+                }
+                ErrorKind::PermissionDenied => {
+                    eprintln!("Path not accessible to program.");
+                }
+                _ => {
+                    println!("Unknown error {error}");
+                }
+            }
 
+            return;
+        }
+    };
+
+    let file_metadata = fs::metadata(Path::new(chosen_directory)).expect("Could not get file metadata");
+    let size = (file_metadata.len() + 32) as usize;
+
+    println!("allocated {size} bytes for chosen file");
+
+    let mut file_contents = Vec::<u8>::with_capacity(size);
+    {
+        let mut reader = BufReader::new(file);
+
+        reader.read_to_end(&mut file_contents).unwrap();
+    }
+
+    println!("allocated memory, press enter to exit");
+
+    let mut s = String::new();
+
+    io::stdin().read_line(&mut s).unwrap();
 }
