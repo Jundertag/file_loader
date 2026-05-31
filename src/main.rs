ff2bc69c95b4ea46da47dcfe35eb79e3fc3b9c28
Jundertag <jaydenmacarthur3@gmail.com>
2026-05-31 10:52:37 -0300

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use std::io::ErrorKind;
 
 fn main() {
     let mut buffer = String::with_capacity(4096);
-    println!("string buffer allocated");
 
     println!("please type the directory to the file to load into heap memory (e.g., \"C:\\Users\\<user>\\Documents\\file.txt\")");
 
@@ -24,4 +23,8 @@ fn main() {
             }
         }
     }
+
+    let chosen_directory = buffer.trim();
+
+
 }
